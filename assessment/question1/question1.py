2512e53b84283d40a1b011af84294b388b88c305
Alex <alexvingg@gmail.com>
2022-05-20 09:33:22 -0300

    add tests

--- assessment/question1/question1.py
+++ assessment/question1/question1.py
@@ -1,6 +1,19 @@
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+from itertools import permutations
+import numpy as np
+
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+
+param = [1,2,3]
+
+list_permutations = list(permutations(param))
+
+output = []
+
+for permutation in list_permutations:
+    output.append(np.asarray(permutation))
+
+
+print(output)
